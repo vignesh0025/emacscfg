9210617573267bb697782b5b206164b0df9ef217
Vignesh D <vignesh0025@gmail.com>
2024-02-15 01:55:41 +0530

    Optimize code

--- elisp/init-options.el
+++ elisp/init-options.el
@@ -11,8 +11,29 @@
 	    ))
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(add-hook 'prog-mode-hook 'save-place-mode)
 
 (setq whitespace-style '(tab-mark)) ; Show only Tab
 (add-hook 'prog-mode-hook 'whitespace-mode)
+
+;; Move Custom-Set-Variables to Different File
+(setq custom-file (concat user-emacs-directory "custom-set-variables.el"))
+(load custom-file 'noerror)
+
+;; Add a newline automatically at the end of the file upon save.
+(setq require-final-newline t)
+
+;; Better Compilation
+(setq-default compilation-always-kill t) ; kill compilation process before starting another
+
+(setq-default compilation-ask-about-save nil) ; save all buffers on `compile'
+
+(setq-default compilation-scroll-output t)
+
+;; So Long mitigates slowness due to extremely long lines.
+;; Currently available in Emacs master branch *only*!
+(when (fboundp 'global-so-long-mode)
+  (global-so-long-mode))
+
 
 (provide 'init-options)
