0e747e067a554485af4902109c68447a0e24ab34
Vignesh D <vignesh0025@gmail.com>
2024-02-15 22:08:55 +0530

    Add native comp support

--- early-init.el
+++ early-init.el
@@ -1,3 +1,6 @@
+(setenv "LIBRARY_PATH" "/home/vignesh/Documents/emacs/gcc-install/lib64/gcc/x86_64-pc-linux-gnu/13.2.0:/home/vignesh/Documents/emacs/gcc-install/lib64")
+(setq native-comp-async-report-warnings-errors nil)
+
 (setq gc-cons-threshold 100000000)
 
 (setq package-enable-at-startup nil)
@@ -7,7 +10,7 @@
 
 (setq site-run-file nil)
 
-(menu-bar-mode 1)
+(menu-bar-mode)
 (unless (and (display-graphic-p) (eq system-type 'darwin))
   (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
@@ -17,7 +20,13 @@
 (scroll-bar-mode -1)
 (set-fringe-mode 10) ; Give some breathing space
 
+(message user-emacs-directory)
+
 (setq user-emacs-directory "~/.emacs.d")
 (setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+;;(if (bound-and-true-p menu-bar-mode)
+;;   (message "menu-bar-mode is on")
+;;  (message "menu-bar-mode is off"))
 
 (provide 'early-init)
---
+++ utils/compile.sh
@@ -0,0 +1,3 @@
+~/Documents/emacs/gcc-install/bin/gcc -L~/Documents/emacs/gcc-install/lib64 -lgccjit gccjit_test.c -o conf
+LD_LIBRARY_PATH=~/Documents/emacs/gcc-install/lib64 LIBRARY_PATH=~/Documents/emacs/gcc-install/lib64/gcc/x86_64-pc-linux-gnu/13.2.0:~/Documents/emacs/gcc-install/lib64 ./conf
+
