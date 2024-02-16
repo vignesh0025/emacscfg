a8af73c7c8be866aeabc32eb61f0eebdabc89ea7
Vignesh D <vignesh0025@gmail.com>
2024-02-17 01:10:28 +0530

    Add more treesit support

--- elisp/init-modemap.el
+++ elisp/init-modemap.el
@@ -2,4 +2,16 @@
 (add-to-list 'auto-mode-alist '("\\.test_suite\\'" . xml-mode))
 (add-to-list 'auto-mode-alist '("\\.haaf\\'" . conf-mode))
 
+(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-ts-mode))
+(add-to-list 'auto-mode-alist '("\\CMakeLists.txt\\'" . cmake-ts-mode))
+
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode))
+(add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
+(add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
+
+;; (treesit-language-available-p 'cmake)
+;; (treesit-language-available-p 'c)
+
 (provide 'init-modemap)
--- elisp/init-treesit.el
+++ elisp/init-treesit.el
@@ -1,16 +1,24 @@
-(setq treesit-language-source-alist
-   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (c "https://github.com/tree-sitter/tree-sitter-c")
-     (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     ))
 
+(defun vd/treesit-config ()
+  (setq treesit-language-source-alist
+	'((bash "https://github.com/tree-sitter/tree-sitter-bash")
+	  (cmake "https://github.com/uyha/tree-sitter-cmake")
+	  (c "https://github.com/tree-sitter/tree-sitter-c")
+	  (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+	  (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+	  (json "https://github.com/tree-sitter/tree-sitter-json")
+	  (make "https://github.com/alemuller/tree-sitter-make")
+	  (python "https://github.com/tree-sitter/tree-sitter-python")
+	  ))
 
-(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
-(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
-(add-to-list 'major-mode-remap-alist
-		'(c-or-c++-mode . c-or-c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode))
+)
 
+(if (treesit-available-p)
+    (vd/treesit-config))
+
+;; (treesit-language-available-p 'c) ; returns if c grammer is available and can be loaded
 
 (provide 'init-treesit)
